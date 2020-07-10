c4bc7ea2a7254737d47a0ab38007c382cb09f009
ahmad essam <ahmad77essam@gmail.com>
2020-07-10 17:08:27 +0200

    some modification on Salary App

---
+++ CalculateTheSalary.java
@@ -0,0 +1,5 @@
+package salaryapp;
+
+public class CalculateTheSalary {
+    
+}
--- OpenScreen.java
+++ OpenScreen.java
@@ -1,14 +1,5 @@
-//package salaryapp;
-//
-//import java.util.Scanner;
-//
-//public class OpenScreen {
-//    private double salary = 0;
-//    
-//    public OpenScreen() {
-//        
-//    }
-//    
-//    Scanner scanner = new Scanner(System.in);
-//        
-//}
+package salaryapp;
+
+public class OpenScreen {
+     
+}
